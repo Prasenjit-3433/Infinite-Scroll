9062834130310153c72e710a9eeab8e69b83c7f4
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-08-13 21:43:35 +0530

    made first api call to Unsplash api to see the response data format

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Infinite-Scroll
 Infinite Scroll --- A most common feature of any social media site....
+
+
+Tech Stack: HTML5, CSS3, Vanilla Js, Unsplash API
+
+How this works:----
+
+![](Unsplash%2BFlowchart.png)
--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing!')
+// Unsplash API
+const noOfPhotos = 10;
+const apiKey = 'xCeQoIgKXxWsCfLv9grx0_cy9wzWQVsKp5jf3GczJoo';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${noOfPhotos}`;
+
+// Get photos from Unsplash Api:
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error here...
+    }
+}
+
+// On Load..
+getPhotos();
